52c4ac6e9c28be6b7a364da6c89ba061b5dec95b
hoangnguyen92dn <hoang.l@nimblehq.co>
2022-07-30 23:04:26 +0700

    [#6] Add getCoins API Service

---
+++ data/src/main/java/co/nimblehq/compose/crypto/data/model/response/CoinItemResponse.kt
@@ -0,0 +1,54 @@
+package co.nimblehq.compose.crypto.data.model.response
+
+import co.nimblehq.compose.crypto.domain.model.CoinItem
+import com.squareup.moshi.Json
+
+data class CoinItemResponse(
+    @Json(name = "id")
+    val id: String?,
+    @Json(name = "symbol")
+    val symbol: String?,
+    @Json(name = "name")
+    val coinName: String?,
+    @Json(name = "current_price")
+    val currentPrice: Int?,
+    @Json(name = "market_cap")
+    val marketCap: Long?,
+    @Json(name = "market_cap_rank")
+    val marketCapRank: Int?,
+    @Json(name = "total_volume")
+    val totalVolume: Long?,
+    @Json(name = "high_24h")
+    val high24h: Long?,
+    @Json(name = "low_24h")
+    val low24h: Long?,
+    @Json(name = "price_change_24h")
+    val priceChange24h: Long?,
+    @Json(name = "price_change_percentage_24h")
+    val priceChangePercentage24h: Int?,
+    @Json(name = "market_cap_change_24h")
+    val marketCapChange24h: Long?,
+    @Json(name = "market_cap_change_percentage_24h")
+    val marketCapChangePercentage24h: Int?,
+    @Json(name = "price_change_percentage_24h_in_currency")
+    val priceChangePercentage24hInCurrency: Long?
+)
+
+private fun CoinItemResponse.toModel() = CoinItem(
+    id = id,
+    symbol = symbol,
+    coinName = coinName,
+    currentPrice = currentPrice,
+    marketCap = marketCap,
+    marketCapRank = marketCapRank,
+    totalVolume = totalVolume,
+    high24h = high24h,
+    low24h = low24h,
+    priceChange24h = priceChange24h,
+    priceChangePercentage24h = priceChangePercentage24h,
+    marketCapChange24h = marketCapChange24h,
+    marketCapChangePercentage24h = marketCapChangePercentage24h,
+    priceChangePercentage24hInCurrency = priceChangePercentage24hInCurrency
+)
+
+fun List<CoinItemResponse>.toModels() = this.map { it.toModel() }
--- data/src/main/java/co/nimblehq/compose/crypto/data/service/ApiService.kt
+++ data/src/main/java/co/nimblehq/compose/crypto/data/service/ApiService.kt
@@ -1,5 +1,18 @@
 package co.nimblehq.compose.crypto.data.service
 
+import co.nimblehq.compose.crypto.data.model.response.CoinItemResponse
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
 interface ApiService {
-    // TODO Add API Service
+    @GET("markets")
+    suspend fun getCoins(
+        @Query("ids") coinIds: List<String>,
+        @Query("vs_currency") currency: String,
+        @Query("price_change_percentage") priceChangePercentage: String,
+        @Query("order") itemOrder: String,
+        @Query("per_page") itemPerPage: Int,
+        @Query("page") page: Int
+    ): Response<List<CoinItemResponse>>
 }
