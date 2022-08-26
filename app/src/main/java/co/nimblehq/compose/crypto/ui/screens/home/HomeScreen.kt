b31cee8d03b9c1a2864515b6e37b1584999b6c31
Wadeewee <suphawadee@nimblehq.co>
2022-08-26 16:25:23 +0700

    investigate scrolling behavior issue

--- app/src/main/java/co/nimblehq/compose/crypto/ui/screens/home/HomeScreen.kt
+++ app/src/main/java/co/nimblehq/compose/crypto/ui/screens/home/HomeScreen.kt
@@ -1,6 +1,7 @@
 package co.nimblehq.compose.crypto.ui.screens.home
 
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
@@ -13,7 +14,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.Dp
 import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.constraintlayout.compose.Dimension
 import co.nimblehq.compose.crypto.R
@@ -28,30 +28,65 @@ import co.nimblehq.compose.crypto.ui.theme.Style.textColor
 @Suppress("FunctionNaming", "LongMethod")
 @Composable
 fun HomeScreen() {
-    val scrollState = rememberScrollState()
-
     Surface {
-        BoxWithConstraints {
-            val screenHeight = maxHeight
+        Column(
+            modifier = Modifier.fillMaxSize()
+        ) {
+            LazyColumn {
+                item {
+                    Text(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(top = Dp16),
+                        text = stringResource(id = R.string.home_title),
+                        textAlign = TextAlign.Center,
+                        style = Style.semiBold24(),
+                        color = MaterialTheme.colors.textColor
+                    )
+                }
 
-            Column(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .verticalScroll(state = scrollState)
-            ) {
-                Text(
-                    modifier = Modifier
-                        .padding(top = Dp16)
-                        .align(Alignment.CenterHorizontally),
-                    text = stringResource(id = R.string.home_title),
-                    textAlign = TextAlign.Center,
-                    style = Style.semiBold24(),
-                    color = MaterialTheme.colors.textColor
-                )
+                item {
+                    PortfolioCard(modifier = Modifier.padding(start = Dp16, top = Dp40, end = Dp16))
+                }
 
-                PortfolioCard(modifier = Modifier.padding(start = Dp16, top = Dp40, end = Dp16))
+                item {
+//                Coins()
+                    LazyRow(
+                        contentPadding = PaddingValues(horizontal = Dp16),
+                        horizontalArrangement = Arrangement.spacedBy(Dp16)
+                    ) {
+                        // TODO: Remove dummy value when work on Integrate.
+                        items(3) { index ->
+                            if (index == 1) CoinItem(true) else CoinItem()
+                        }
+                    }
+                }
 
-                Coins(screenHeight = screenHeight)
+                item {
+                    Box(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(start = Dp16, top = Dp24, end = Dp16, bottom = Dp16)
+                    ) {
+                        Text(
+                            modifier = Modifier.align(alignment = Alignment.TopStart),
+                            text = stringResource(id = R.string.home_trending_title),
+                            style = Style.medium16(),
+                            color = MaterialTheme.colors.textColor,
+                        )
+
+                        SeeAll(
+                            modifier = Modifier
+                                .align(alignment = Alignment.CenterEnd)
+                                .clickable(onClick = { /* TODO: Update on Integrate ticket */ })
+                        )
+                    }
+                }
+
+                // TODO: Remove dummy value when work on Integrate.
+                items(7) {
+                    TrendingItem()
+                }
             }
         }
     }
@@ -59,21 +94,17 @@ fun HomeScreen() {
 
 @Suppress("FunctionNaming", "LongMethod", "MagicNumber")
 @Composable
-private fun Coins(screenHeight: Dp) {
+private fun Coins() {
     ConstraintLayout(
         modifier = Modifier
             .fillMaxWidth()
-            .height(screenHeight)
-            .padding(vertical = Dp52)
+            .padding(top = Dp52)
     ) {
 
         val (
             myCoinsTitle,
             seeAllMyCoins,
-            myCoins,
-            trendingTitle,
-            seeAllTrending,
-            trending
+            myCoins
         ) = createRefs()
 
         Text(
@@ -113,46 +144,6 @@ private fun Coins(screenHeight: Dp) {
             // TODO: Remove dummy value when work on Integrate.
             items(3) { index ->
                 if (index == 1) CoinItem(true) else CoinItem()
-            }
-        }
-
-        Text(
-            modifier = Modifier
-                .constrainAs(trendingTitle) {
-                    top.linkTo(myCoins.bottom, margin = Dp24)
-                    start.linkTo(parent.start)
-                    width = Dimension.preferredWrapContent
-
-                }
-                .padding(start = Dp16),
-            text = stringResource(id = R.string.home_trending_title),
-            style = Style.medium16(),
-            color = MaterialTheme.colors.textColor
-        )
-
-        SeeAll(
-            modifier = Modifier
-                .clickable(onClick = { /* TODO: Update on Integrate ticket */ })
-                .constrainAs(seeAllTrending) {
-                    top.linkTo(trendingTitle.top)
-                    end.linkTo(parent.end)
-                    width = Dimension.preferredWrapContent
-                }
-                .padding(end = Dp16)
-        )
-
-        LazyColumn(
-            modifier = Modifier
-                .constrainAs(trending) {
-                    top.linkTo(trendingTitle.bottom, margin = Dp16)
-                    start.linkTo(parent.start)
-                }
-                .padding(horizontal = Dp16),
-            verticalArrangement = Arrangement.spacedBy(Dp16)
-        ) {
-            // TODO: Remove dummy value when work on Integrate.
-            items(4) { index ->
-                if (index == 1) TrendingItem(true) else TrendingItem()
             }
         }
     }
