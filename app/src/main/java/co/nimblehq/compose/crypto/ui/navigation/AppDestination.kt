9791275e90128578e227ea17647d7651a7a5a801
Luong Vo (Lucas) <vodailuong@gmail.com>
2022-10-04 01:34:54 +0700

    [#42] Create default route value as empty + add comment for route definition

--- app/src/main/java/co/nimblehq/compose/crypto/ui/navigation/AppDestination.kt
+++ app/src/main/java/co/nimblehq/compose/crypto/ui/navigation/AppDestination.kt
@@ -4,7 +4,7 @@ import androidx.navigation.*
 
 const val KEY_COIN_ID = "coinId"
 
-sealed class AppDestination(val route: String) {
+sealed class AppDestination(val route: String = "") {
 
     open val arguments: List<NamedNavArgument> = emptyList()
 
@@ -12,10 +12,17 @@ sealed class AppDestination(val route: String) {
 
     //====================================================//
 
-    object Up : AppDestination("")
+    object Up : AppDestination()
 
     object Home : AppDestination("home")
 
+    /**
+     * We can define route as "coin/details" without "coinId" parameter because we're passing it as argument already.
+     * So either passing "coinId" via arguments or passing it via route.
+     *
+     * We keep passing "coinId" in both route and arguments for this destination to give example of navigation concept
+     * about how to build a destination with parameters.
+     */
     object CoinDetail : AppDestination("coin/{$KEY_COIN_ID}") {
 
         override val arguments = listOf(
