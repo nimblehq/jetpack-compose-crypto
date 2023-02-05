83c2f885bc801369a0b7bb2eb3875c7b253c9328
doandinh <doan@nimblehq.co>
2023-02-05 23:04:56 +0700

    [77] Update app bar

--- app/src/main/java/co/nimblehq/compose/crypto/ui/screens/detail/Appbar.kt
+++ app/src/main/java/co/nimblehq/compose/crypto/ui/screens/detail/Appbar.kt
@@ -10,6 +10,7 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import co.nimblehq.compose.crypto.R
 import co.nimblehq.compose.crypto.core.theme.AppTheme
+import co.nimblehq.compose.crypto.core.theme.ComposeTheme
 
 @Composable
 fun Appbar(
